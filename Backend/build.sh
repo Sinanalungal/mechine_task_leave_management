7714bedba2a7da96ffc79b902348a57ae8b42173
sinan <sinanalungal012@gmail.com>
2024-12-07 15:45:47 +0530

    managing build commandes in backend

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+
+
+echo "Applying migrations..."
+python manage.py migrate --noinput
+
+# Conditionally create a superuser if the CREATE_SUPERUSER environment variable is set
 if [[ $CREATE_SUPERUSER ]];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  echo "Creating superuser..."
+  python manage.py createsuperuser --no-input
 fi
+
+
+echo "Build script completed successfully."
